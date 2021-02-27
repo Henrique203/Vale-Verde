04843f001b193a5c44e15e63d6b4f54f3ad4a47c
Henrique203 <60670444+Henrique203@users.noreply.github.com>
2021-02-27 20:24:22 -0300

    Add files via upload

---
+++ concluido.php
@@ -0,0 +1,1 @@
+<h1> Ficha submetida com sucesso </h1>
---
+++ form-empresa.php
@@ -0,0 +1,19 @@
+<h1> MINE HORTA VALE VERDE </h1>
+<form method="post" action="salva-formulario.php">
+    <p>Nº: <br /><input type="text" name="num" size="80" value=""></p>
+    <p>Data: <br /><input type="date" name="data" size="80" value=""></p>
+    <p>Firma: <br /><input type="text" name="firma" size="80" ></p>
+    <p>Endereço: <br /><input type="text" name="ender" size="80" value=""></p>
+    <p>Complemento: <br /><input type="text" name="compl" size="80" value=""></p>
+    <p>Bairro: <br /><input type="text" name="bairro" size="80" value=""></p>
+    <p>Cidade: <br /><input type="text" name="cidade" size="80" value=""></p>
+    <p>Estado: <br /><input type="text" name="estado" size="80" value=""></p>
+    <p>CEP: <br /><input type="text" name="cep" size="80" value=""></p>
+    <p>CNPJ: <br /><input type="text" name="cnpj" size="80" value=""></p>
+    <p>Inscrição: <br /><input type="text" name="insc" size="80" value=></p>
+    <p>Telefones: <br /><input type="text" name="tels" size="80" ></p>
+    <p>Celular: <br /><input type="text" name="cel" size="80" ></p>
+    <p>E-mail: <br /><input type="text" name="email" size="80" ></p>
+    <p>OBS: <br /><textarea name="obs" rows="6" cols="80"></textarea></p>
+    <p><button type="submit">Envia</button></p>
+</form>
